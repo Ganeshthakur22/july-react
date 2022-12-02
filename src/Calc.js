aa3c686278ce5580e4c8ed9c7a269f65c57353b9
ganesh <thakurgnesh22@gmail.com>
2022-12-02 17:23:14 +0530

    first time

---
+++ src/Calc.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Calc = () => {
+  return (
+    <div>
+      
+      <input type={"text"}  className={"form-control"}></input> <br></br>
+
+      <input type={"text"}  className={"form-control"}></input> <br></br>
+
+      <input type={"text"}  className={"form-control"}></input> <br></br>
+
+      <button className={"btn btn-success"}>
+buttion
+      </button>
+    </div>
+  )
+}
+
+export default Calc
